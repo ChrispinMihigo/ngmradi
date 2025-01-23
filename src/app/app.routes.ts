69baa2733200a0d03c47987109c66928680d505a
Chrispin Mihigo <mihigochrispin@gmail.com>
2025-01-23 08:55:48 +0200

    Sauvegarde des modifications avant la mise à jour Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,61 @@
 import { Routes } from '@angular/router';
+import { APP_NAME } from './app.constants';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    title: `Connexion - ${APP_NAME}`,
+    loadComponent: () => import('./pages/login/login.component'),
+  },
+  {
+    path: '',
+    title: `${APP_NAME}`,
+    loadComponent: () => import('./pages/home/home.component'),
+    children: [
+      {
+        path: 'projects',
+        title: `Projects - ${APP_NAME}`,
+        loadComponent: () => import('./pages/home/projects/projects.component'),
+      },
+      {
+        path: 'contributors',
+        title: `Contributeurs - ${APP_NAME}`,
+        loadComponent: () =>
+          import('./pages/home/contributors/contributors.component'),
+        children: [
+          {
+            path: 'active',
+            title: `Contributeurs actifs - ${APP_NAME}`,
+            loadComponent: () =>
+              import('./pages/home/contributors/active/active.component'),
+          },
+          {
+            path: 'archived',
+            title: `contriuteurs archivés - ${APP_NAME}`,
+            loadComponent: () =>
+              import('./pages/home/contributors/archived/archived.component'),
+          },
+          {
+            path: '',
+            redirectTo: 'active',
+            pathMatch: 'full',
+          },
+        ],
+      },
+      {
+        path: '',
+        redirectTo: 'projects',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: 'project/:id',
+    title: `Chargement du projet... - ${APP_NAME}`,
+    loadComponent: () => import('./pages/projet/projet.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'projects',
+  },
+];
